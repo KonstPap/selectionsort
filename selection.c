87251f9cb2cc1149626cb9c278b5d14ec17d4082
KonstPap <32640290+KonstPap@users.noreply.github.com>
2019-10-31 19:04:32 +0200

    Create selection.c

---
+++ selection.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[100],i,j,temp,maxpos;
+    for(i=0;i<100;i++)
+        arr[i]=rand();
+    for(i=100-1;i>0;i--)
+    {
+        maxpos=0;
+        for(j=1;j<=i;j++)
+        {
+           
+    }
+    for(i=0;i<100;i++)
+        printf("%d\n",arr[i]);
+    return 0;
+}
